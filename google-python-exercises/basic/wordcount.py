a6f16bd70b367673457555bfb4a527572079b5af
anhngduc <anh_nguyen.1991@outlook.com>
2017-06-25 19:32:53 +0900

    change file: _worldcount.py

--- google-python-exercises/basic/wordcount.py
+++ google-python-exercises/basic/wordcount.py
@@ -44,9 +44,40 @@ import sys
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
+def word_Count(filename):
+  f= open(filename,'rU')
+  text=f.read() 
+  text=text.lower()
+  list=text.split();  
+  md={}
+  for s in list:
+    if s in md:
+      md[s]+=1;
+    else:
+      md[s]=1
+  #md=sorted(md)
+  
+  return md
 
+def print_words(filename):
+ d=word_Count(filename)
+ for st in sorted(d.keys()):
+    print st + ": " + str(d[st])
+  
 ###
 
+
+##printtop function
+def print_top(filename):
+  d=word_Count(filename)
+  li=sorted(d.items(),key=comp,reverse=True)[:20]
+  
+  for st in li:
+    print st[0] + ": " + str(st[1])
+  
+  
+def comp(tp):
+  return tp[-1]
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
