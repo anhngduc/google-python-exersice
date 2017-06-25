62776a28bacf5beb15ac7529db1cd52f67acfd81
anhngduc <anh_nguyen.1991@outlook.com>
2017-06-25 23:46:51 +0900

    Add files via upload
    not fully finish

--- google-python-exercises/copyspecial/copyspecial.py
+++ google-python-exercises/copyspecial/copyspecial.py
@@ -17,9 +17,27 @@ import commands
 
 # +++your code here+++
 # Write functions and modify main() to call them
+def Specialfile(dir):
+  filenames=os.listdir(dir)
+  files=[]
+  for filename in filenames:
+    if re.search(r'__\w+__',filename):
+      print 'fn:',filename
+      path=os.path.join(dir,filename)
+      abspath=os.path.abspath(path)
+      files.append(abspath)
+      print abspath
+  return  files
+
+# Copy file to dir
+def toDir(files,targetdir):  
+  print 'dir:',targetdir  
+  
+  for filepath in files:
+    shutil.copy(filepath,targetdir)
 
 
-
+  
 def main():
   # This basic command line argument parsing code is provided.
   # Add code to call your functions below.
@@ -30,15 +48,25 @@ def main():
   if not args:
     print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
     sys.exit(1)
-
+  
+  if len(args) ==1:
+    filelist=Specialfile(args[0])
   # todir and tozip are either set from command line
   # or left as the empty string.
   # The args array is left just containing the dirs.
   todir = ''
-  if args[0] == '--todir':
+  if args[0] == '--todir':    
     todir = args[1]
+    filelist=Specialfile(args[-1])
+    path=os.path.join(args[-1],todir)
+    abspath=os.path.abspath(path)    
+    if not(os.path.exists(abspath)) : 
+      os.mkdir(abspath)
+    
+    print 'k:',abspath
     del args[0:2]
-
+    toDir(filelist,abspath)
+  
   tozip = ''
   if args[0] == '--tozip':
     tozip = args[1]
